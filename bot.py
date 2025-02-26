57d12704248491df13d64c07d542621172b5ba54
SeoulDroid99100 <zekey8249+1@gmail.com>
2025-02-26 16:03:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,9 +32,9 @@ import os
 @dataclass(frozen=True)
 class Config:
     API_ID: int = int(os.environ["API_ID"])
-    API_HASH: str = os.environ["API_HASH"])
-    BOT_TOKEN: str = os.environ["BOT_TOKEN"])
-    POSTGRES_URI: str = os.environ["POSTGRES_URI"])
+    API_HASH: str = os.environ["API_HASH"]
+    BOT_TOKEN: str = os.environ["BOT_TOKEN"]
+    POSTGRES_URI: str = os.environ["POSTGRES_URI"]
     POOL_SIZE: int = 15
     ADMINS: List[int] = field(default_factory=lambda: [6656275515])
     SESSION_DIR: str = os.environ.get("SESSION_DIR", "/app/sessions") # Get session dir from env, default to /app/sessions
